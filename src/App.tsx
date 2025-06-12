e46c5bc6b518019f22f50d44f0202fc70af15cca
Abdullah-Aurabiq <hack1the1thor@gmail.com>
2025-06-12 10:22:14 +0000

    SideBar Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <ThemeProvider>
       <Router>
-        <div className="min-h-screen bg-white text-gray-900 dark:bg-gray-800 dark:text-white transition-colors duration-300">
-          <Header />
-          <main className="px-4 py-6">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </main>
+        <div className="min-h-screen bg-white text-gray-900 dark:bg-gray-800 dark:text-white transition-colors duration-300 flex">
+          <Sidebar />
+          <div className="flex-1">
+            <Header />
+            <main className="px-4 py-6">
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </main>
+          </div>
         </div>
       </Router>
     </ThemeProvider>
